c9c8517f87e8926f03ab6f4c710e7f8ebdef67c7
Deema-Thneibi <deemathneibi@gmail.com>
2026-03-31 01:38:32 +0300

    fail ci on security warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-export default [
+export default defineConfig([
   {
     ignores: ["coverage/**", "dist/**", "node_modules/**"],
   },
   {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tsParser,
-      sourceType: "module",
-      ecmaVersion: "latest",
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": "error",
-    },
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
   },
-];
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+]);
